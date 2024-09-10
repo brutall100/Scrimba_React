213d73b197b3b155b86848abbba7b63fd3d8b9ac
Aldas <brutall100@gmail.com>
2024-09-10 21:32:48 +0300

    React simple

--- first-scrimba-project/index.js
+++ first-scrimba-project/index.js
@@ -1,1 +1,28 @@
-ReactDOM.render(<h1>HELOOO</h1>, document.getElementById('root'));
+function Navbar() {
+    return (
+        <nav>
+            <h1>Hello World</h1>
+        </nav>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <p>This is where most of my content will go...</p>
+        </main>
+    )
+}
+
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
+
+
+// ! REACT panaudojimas be instaliuotu Biblioteku
+// Bus metamos klaidos, bet kodas veiks. 
